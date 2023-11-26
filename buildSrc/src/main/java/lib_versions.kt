b1a6e4b3da15adf0b35ba57d1866cbaea6c2c775
Timo Drick <timo.drick@7p-group.com>
2023-11-27 00:48:32 +0100

    update libs

--- buildSrc/src/main/java/lib_versions.kt
+++ buildSrc/src/main/java/lib_versions.kt
@@ -7,11 +7,11 @@ object Versions {
     const val compileSdk = 34
 
     const val coreKtx = "1.12.0"
-    const val lifecycle = "2.7.0-alpha02"
+    const val lifecycle = "2.7.0-rc01"
     const val datastore = "1.0.0"
 
     const val composeCompiler = "1.5.3"
-    const val activityCompose = "1.8.0-rc01"
+    const val activityCompose = "1.8.1"
     const val composeBom = "2023.09.02"
     const val composeDesktop = "1.5.2"
     const val accompanist = "0.32.0" //comose ui 1.5.x
