3ab41476522ea97c6fe7cb3ee726b920c7eeeec0
Timo Drick <timo.drick@7p-group.com>
2024-02-25 14:57:41 +0100

    Added camera cutout.

---
+++ edgetoedgepreview/src/main/java/de/drick/compose/edgetoedgepreview/templates/camera_cutout.kt
@@ -0,0 +1,99 @@
+package de.drick.compose.edgetoedgepreview.templates
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lens
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+enum class CameraCutoutMode {
+    None, Middle, Start, End
+}
+
+@Preview(name = "Portrait")
+@Preview(name = "Landscape", device = "spec:parent=pixel_5,orientation=landscape")
+@Composable
+fun PreviewCameraCutoutVertical() {
+    val isLandscape = LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
+    val cutoutSize = 80.dp
+    if (isLandscape) {
+        CameraCutout(
+            modifier = Modifier.size(cutoutSize, 400.dp),
+            cutoutMode = CameraCutoutMode.Middle,
+            isVertical = true
+        )
+    } else {
+        CameraCutout(
+            modifier = Modifier.size(400.dp, cutoutSize),
+            cutoutMode = CameraCutoutMode.Middle,
+            isVertical = false
+        )
+    }
+}
+
+@Composable
+fun CameraCutout(
+    modifier: Modifier = Modifier,
+    cutoutMode: CameraCutoutMode = CameraCutoutMode.Middle,
+    isVertical: Boolean = false,
+    cutoutSize: Dp = 24.dp
+) {
+    if (cutoutMode == CameraCutoutMode.None) return
+    if (isVertical) {
+        val alignment = when (cutoutMode) {
+            CameraCutoutMode.None -> Arrangement.Center
+            CameraCutoutMode.Middle -> Arrangement.Center
+            CameraCutoutMode.Start -> Arrangement.Top
+            CameraCutoutMode.End -> Arrangement.Bottom
+        }
+        Column(
+            modifier = modifier
+                .padding(4.dp)
+                .width(cutoutSize)
+                .fillMaxHeight(),
+            verticalArrangement = alignment,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Default.Lens,
+                contentDescription = "Camera lens",
+                )
+        }
+    } else {
+        val alignment = when (cutoutMode) {
+            CameraCutoutMode.None -> Arrangement.Center
+            CameraCutoutMode.Middle -> Arrangement.Center
+            CameraCutoutMode.Start -> Arrangement.Start
+            CameraCutoutMode.End -> Arrangement.End
+        }
+        Row(
+            modifier = modifier
+                .padding(4.dp)
+                .height(cutoutSize)
+                .fillMaxWidth(),
+            horizontalArrangement = alignment,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.Lens,
+                contentDescription = "Camera lens",
+
+                )
+        }
+    }
+}
--- edgetoedgepreview/src/main/java/de/drick/compose/edgetoedgepreview/templates/edge_to_edge_template.kt
+++ edgetoedgepreview/src/main/java/de/drick/compose/edgetoedgepreview/templates/edge_to_edge_template.kt
@@ -1,11 +1,12 @@
 package de.drick.compose.edgetoedgepreview.templates
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
@@ -16,12 +17,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.core.view.WindowInsetsCompat
 
@@ -30,12 +33,17 @@ class NavigationPreviewProvider : PreviewParameterProvider<NavigationMode> {
 }
 
 enum class NavigationMode {
-    Gesture, ThreeButton
+    Gesture,
+    ThreeButton
 }
 
 @Composable
 fun EdgeToEdgeTemplate(
     navMode: NavigationMode = NavigationMode.ThreeButton,
+    cameraCutoutMode: CameraCutoutMode = CameraCutoutMode.Middle,
+    isInvertedOrientation: Boolean = false,// in landscape mode it would be that the camera cutout is
+                                           // on the right and navigation buttons will be on the left
+    showInsetsBorder: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val isLandscape = LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
@@ -43,37 +51,91 @@ fun EdgeToEdgeTemplate(
 
     var statusBarHeight by remember { mutableIntStateOf(0) }
     var navigationBarSize by remember { mutableIntStateOf(0) }
+    var cameraCutoutSize by remember { mutableIntStateOf(0) }
 
-    val isBottomNavigationBar = navMode == NavigationMode.Gesture || isLandscape.not()
+    val navigationPos = when {
+        navMode == NavigationMode.Gesture -> InsetPos.BOTTOM
+        isLandscape && isInvertedOrientation -> InsetPos.LEFT
+        isLandscape -> InsetPos.RIGHT
+        else -> InsetPos.BOTTOM
+    }
+    val cameraCutoutPos = when {
+        isLandscape.not() && isInvertedOrientation -> InsetPos.BOTTOM
+        isLandscape && isInvertedOrientation.not() -> InsetPos.LEFT
+        isLandscape -> InsetPos.RIGHT
+        else -> InsetPos.TOP
+    }
 
     val windowInsetsState = rememberWindowInsetsState()
-    LaunchedEffect(statusBarHeight, navigationBarSize) {
+    LaunchedEffect(statusBarHeight, navigationBarSize, cameraCutoutSize) {
         windowInsetsState.update {
             setInset(InsetPos.TOP, WindowInsetsCompat.Type.statusBars(), statusBarHeight)
             setInset(
-                pos = if (isBottomNavigationBar) InsetPos.BOTTOM else InsetPos.RIGHT,
+                pos = navigationPos,
                 type = WindowInsetsCompat.Type.navigationBars(),
                 size = navigationBarSize
             )
+            if (cameraCutoutMode != CameraCutoutMode.None) {
+                setInset(
+                    pos = cameraCutoutPos,
+                    type = WindowInsetsCompat.Type.displayCutout(),
+                    size = cameraCutoutSize
+                )
+            }
         }
     }
     Box(Modifier.fillMaxSize()) {
+        val borderModifier = if (showInsetsBorder) Modifier.border(2.dp, Color.Red) else Modifier
+        val cameraCutoutAlignment = when(cameraCutoutPos) {
+            InsetPos.LEFT -> AbsoluteAlignment.CenterLeft
+            InsetPos.TOP -> AbsoluteAlignment.TopLeft
+            InsetPos.RIGHT -> AbsoluteAlignment.CenterRight
+            InsetPos.BOTTOM -> AbsoluteAlignment.BottomLeft
+        }
+        CameraCutout(
+            modifier = Modifier
+                .zIndex(1001f)
+                .align(cameraCutoutAlignment)
+                .then(borderModifier)
+                .onSizeChanged {
+                    cameraCutoutSize = if (isLandscape) it.width else it.height
+                },
+            cutoutMode = cameraCutoutMode,
+            isVertical = isLandscape
+        )
         StatusBar(
             modifier = Modifier
-                .windowInsetsPadding(WindowInsets.navigationBars.only(WindowInsetsSides.Right))
+                .windowInsetsPadding(WindowInsets.navigationBars.only(WindowInsetsSides.Horizontal))
+                .windowInsetsPadding(WindowInsets.displayCutout.only(WindowInsetsSides.Horizontal))
                 .zIndex(1000f)
                 .align(Alignment.TopCenter)
+                .then(borderModifier)
                 .onSizeChanged {
                     statusBarHeight = it.height
                 },
             isDarkMode = isDarkMode
         )
+        val navigationBarAlignment = when(navigationPos) {
+            InsetPos.LEFT -> AbsoluteAlignment.CenterLeft
+            InsetPos.TOP -> AbsoluteAlignment.TopLeft
+            InsetPos.RIGHT -> AbsoluteAlignment.CenterRight
+            InsetPos.BOTTOM -> AbsoluteAlignment.BottomLeft
+        }
         NavigationBar(
             modifier = Modifier
+                .windowInsetsPadding(WindowInsets.displayCutout.only(
+                    WindowInsetsSides.Horizontal + WindowInsetsSides.Bottom
+                ))
                 .zIndex(1000f)
-                .align(if (isBottomNavigationBar) Alignment.BottomCenter else Alignment.CenterEnd)
+                .align(navigationBarAlignment)
+                .then(borderModifier)
                 .onSizeChanged {
-                    navigationBarSize = if (isBottomNavigationBar) it.height else it.width
+                    val cutoutPadding = if (cameraCutoutPos == InsetPos.BOTTOM) cameraCutoutSize else 0
+                    navigationBarSize = if (navigationPos == InsetPos.BOTTOM) {
+                        it.height + cutoutPadding
+                    } else {
+                        it.width
+                    }
                 },
             isVertical = isLandscape,
             isDarkMode = isDarkMode,
@@ -81,26 +143,4 @@ fun EdgeToEdgeTemplate(
         )
         content()
     }
-}
-
-@Composable
-fun UseCase() {
-    WindowInsetsEmulator(
-        {
-
-        },
-        {
-
-        }
-    ) {
-        Box(modifier = Modifier.fillMaxSize().background(Color.Green))
-    }
-}
-
-@Composable
-fun WindowInsetsEmulator(
-    vararg insets: @Composable () -> Unit,
-    content: @Composable () -> Unit,
-) {
-
 }
